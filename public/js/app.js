47cba6af5f6fea268af386dbc1f3e66eb76658ec
John Glenn Eligio <glenneligio@gmail.com>
2022-06-14 11:13:50 +0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,20 +5,21 @@ const messageTwo = document.querySelector("#message-2");
 
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const location = search.value;
+
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          messageTwo.textContent = "";
-          return;
-        }
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.forecast;
-      });
-    }
-  );
+
+  fetch("weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+        return;
+      }
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
+    });
+  });
 });
